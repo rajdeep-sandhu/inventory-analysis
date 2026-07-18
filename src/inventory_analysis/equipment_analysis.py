5697f59ac7777723768e5a4823901ef6c1ed33b4
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-07-18 22:18:15 +0000

    create equipment_analysis.py

---
+++ src/inventory_analysis/equipment_analysis.py
@@ -0,0 +1,13 @@
+import marimo
+
+__generated_with = "0.23.14"
+app = marimo.App(width="full", app_title="Equipment Analysis")
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
