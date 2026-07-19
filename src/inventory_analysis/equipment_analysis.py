248deff0e46e84315d808416adbbbf7297e8d5f5
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-07-19 09:39:36 +0000

    add file_to_df()

--- src/inventory_analysis/equipment_analysis.py
+++ src/inventory_analysis/equipment_analysis.py
@@ -3,6 +3,11 @@ import marimo
 __generated_with = "0.23.14"
 app = marimo.App(width="full", app_title="Equipment Analysis")
 
+with app.setup:
+    from io import BytesIO
+
+    import polars as pl
+
 
 @app.cell(hide_code=True)
 def _(mo):
@@ -41,9 +46,34 @@ def _(mo):
     return (get_data_file,)
 
 
+@app.function
+def file_to_df(file_input) -> dict:
+    """
+    Returns the filename and a polars dataframe from a .xlsx file uploaded via mo.ui.file.
+
+    params:
+    file_input: The file input element.
+    """
+
+    # Get first result for single file upload
+    result = file_input.value[0]
+    filename: str = result.name
+    content: bytes = result.contents
+    df: pl.DataFrame = pl.read_excel(source=BytesIO(content))
+
+    return {"filename": filename, "data": df}
+
+
 @app.cell
 def _(get_data_file):
-    get_data_file()
+    file_input = get_data_file()
+    file_input
+    return (file_input,)
+
+
+@app.cell
+def _(file_input):
+    file_to_df(file_input)
     return
 
 
