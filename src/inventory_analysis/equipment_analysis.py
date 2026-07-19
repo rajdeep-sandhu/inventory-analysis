6d965bb70a906c23668143446499d63456b0358c
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-07-19 08:00:24 +0000

    add get_data_file()

--- src/inventory_analysis/equipment_analysis.py
+++ src/inventory_analysis/equipment_analysis.py
@@ -19,5 +19,33 @@ def _():
     return (mo,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Load Equipment Transactions
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    def get_data_file():
+        file_input = mo.ui.file(
+            label="Upload Equipment Transaction Data (`.xlsx`)",
+            filetypes=[".xlsx"],
+            multiple=False,
+        )
+
+        return file_input
+
+    return (get_data_file,)
+
+
+@app.cell
+def _(get_data_file):
+    get_data_file()
+    return
+
+
 if __name__ == "__main__":
     app.run()
